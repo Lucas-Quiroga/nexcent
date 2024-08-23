f72dec3babbfa75481498017b512613115a3a038
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2024-08-23 13:45:39 -0300

    I18N

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@components": "/src/components",
+        "@layouts": "/src/layouts",
+        "@pages": "/src/pages",
+        "@public": "/public",
+      },
+    },
+  },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "nav.service": "Service",
+    "nav.feature": "Feature",
+    "nav.product": "Product",
+    "nav.testimonial": "Testimonial",
+    "nav.faq": "FAQ",
+  },
+  es: {
+    "nav.service": "Servicio",
+    "nav.feature": "Característica",
+    "nav.product": "Producto",
+    "nav.testimonial": "Testimonio",
+    "nav.faq": "Preguntas frecuentes",
+  },
+} as const;
